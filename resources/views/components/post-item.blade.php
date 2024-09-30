e3fa9f2353dd64bf4d0f668091a0ee85bff74125
Sevalino Elfata <sevalinoelfata8@gmail.com>
2024-09-30 20:39:23 +0700

    before production

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -75,79 +75,84 @@
                 </div>
             @endif
         @endif
-    </div>
-    <div class="btn-group align-end" role="group" style="column-gap: 5px;">
-        <form action="{{ $post->isRespondedBy(Auth::user(), 'upvote') ? route('post.response.destroy') : route('post.response') }}" method="post" style="display: inline-block;">
-            @csrf
-            @if($post->isRespondedBy(Auth::user(), 'upvote'))
-                @method('DELETE')
-            @else
-                @method('POST')
-            @endif
-            <input type="hidden" name="post_id" value="{{ $post->id }}">
-            <input type="hidden" name="action" value="upvote">
-            <button type="submit" class="btn btn-outline-primary {{ $post->isRespondedBy(Auth::user(), 'upvote') ? 'active' : '' }}" style="display: inline-block;"><span class="material-icons align-middle">arrow_upward</span>{{ number_format((int) $post->upvote_count, 0, '.', ' ') > 1000000 ? number_format((int) $post->upvote_count / 1000000, 1) . 'm' : (number_format((int) $post->upvote_count, 0, '.', ' ') > 1000 ? number_format((int) $post->upvote_count / 1000, 1) . 'k' : $post->upvote_count ?? 0) }}</button>
-        </form>
-        <form action="{{ $post->isRespondedBy(Auth::user(), 'downvote') ? route('post.response.destroy') : route('post.response') }}" method="post" style="display: inline-block;">
-            @csrf
-            @if($post->isRespondedBy(Auth::user(), 'downvote'))
-                @method('DELETE')
-            @else
-                @method('POST')
-            @endif
-            <input type="hidden" name="post_id" value="{{ $post->id }}">
-            <input type="hidden" name="action" value="downvote">
-            <button type="submit" class="btn btn-outline-danger {{ $post->isRespondedBy(Auth::user(), 'downvote') ? 'active' : '' }}" style="display: inline-block;"><span class="material-icons align-middle">arrow_downward</span>{{ number_format((int) $post->downvote_count, 0, '.', ' ') > 1000000 ? number_format((int) $post->downvote_count / 1000000, 1) . 'm' : (number_format((int) $post->downvote_count, 0, '.', ' ') > 1000 ? number_format((int) $post->downvote_count / 1000, 1) . 'k' : $post->downvote_count ?? 0) }}</button>
-        </form>
-        <form action="{{ $post->isRespondedBy(Auth::user(), 'star') ? route('post.response.destroy') : route('post.response') }}" method="post" style="display: inline-block;">
-            @csrf
-            @if($post->isRespondedBy(Auth::user(), 'star'))
-                @method('DELETE')
-            @else
-                @method('POST')
-            @endif
-            <input type="hidden" name="post_id" value="{{ $post->id }}">
-            <input type="hidden" name="action" value="star">
-            <button type="submit" class="btn btn-outline-warning {{ $post->isRespondedBy(Auth::user(), 'star') ? 'active' : '' }}" style="display: inline-block;"><span class="material-icons align-middle">star</span>{{ number_format((int) $post->star_count, 0, '.', ' ') > 1000000 ? number_format((int) $post->star_count / 1000000, 1) . 'm' : (number_format((int) $post->star_count, 0, '.', ' ') > 1000 ? number_format((int) $post->star_count / 1000, 1) . 'k' : $post->star_count ?? 0) }}</button>
-        </form>
-        <form action="{{ $post->isRespondedBy(Auth::user(), 'save') ? route('post.response.destroy') : route('post.response') }}" method="post" style="display: inline-block;">
-            @csrf
-            @if($post->isRespondedBy(Auth::user(), 'save'))
-                @method('DELETE')
-            @else
-                @method('POST')
-            @endif
-            <input type="hidden" name="post_id" value="{{ $post->id }}">
-            <input type="hidden" name="action" value="save">
-            <button type="submit" class="btn btn-outline-success {{ $post->isRespondedBy(Auth::user(), 'save') ? 'active' : '' }}" style="display: inline-block;"><span class="material-icons align-middle">bookmark</span>{{ number_format((int) $post->save_count, 0, '.', ' ') > 1000000 ? number_format((int) $post->save_count / 1000000, 1) . 'm' : (number_format((int) $post->save_count, 0, '.', ' ') > 1000 ? number_format((int) $post->save_count / 1000, 1) . 'k' : $post->save_count ?? 0) }}</button>
-        </form>
     </div>        
-    <p class="text-justify mb-4"><x-linkify-content :content="$post->body" /></p>    <div class="d-flex justify-content-end mt-4">        
-        @if($posts)
-        <a href="/post/{{ $post->id }}" class="btn btn-outline-primary me-3 align-middle d-flex align-items-center">
-            <span class="material-icons align-middle">description</span>
-            <span class="align-middle">detail</span>
-        </a>
+    @if ($posts)
+    <p class="text-justify mb-4"><x-linkify-content :content="Str::limit($post->body, 75)" />
+        @if (strlen($post->body) > 75)
+        <a class="text-secondary text-decoration-none" href="/post/{{ $post->id }}">More Detail</a>
         @endif
-        @if ($commentDropdown)
-        <button class="btn btn-outline-primary me-3 align-middle d-flex align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#commentSection-{{ $post->id }}" aria-expanded="false" aria-controls="commentSection-{{ $post->id }}">
-            <span class="material-icons align-middle">comment</span>
-            <span class="align-middle">Komentar</span>
-        </button>
-        @endif
-        @if ($post->user_id == Auth::id())
-        <form action="{{ url('/post/delete') }}" method="post" class="d-inline">
-            @csrf
-            @method('DELETE')
-            <input type="hidden" name="post" value="{{ $post->id }}">
-            <button type="submit" class="btn btn-outline-danger align-middle d-flex align-items-center">
-                <span class="material-icons align-middle">delete_outline</span>
-                <span class="align-middle">Hapus</span>
-            </button>
-        </form>
-        @endif
-    </div>
-
+    </p> 
+    @else
+    <p class="text-justify mb-4"><x-linkify-content :content="$post->body" /></p>  
+    @endif       
+        <div class="d-flex justify-content-end mt-4">
+            <div class="btn-group align-end me-3" role="group" style="column-gap: 2px;">
+                <form action="{{ $post->isRespondedBy(Auth::user(), 'upvote') ? route('post.response.destroy') : route('post.response') }}" method="post" style="display: inline-block;">
+                    @csrf
+                    @if($post->isRespondedBy(Auth::user(), 'upvote'))
+                        @method('DELETE')
+                    @else
+                        @method('POST')
+                    @endif
+                    <input type="hidden" name="post_id" value="{{ $post->id }}">
+                    <input type="hidden" name="action" value="upvote">
+                    <button type="submit" class="btn btn-outline-primary {{ $post->isRespondedBy(Auth::user(), 'upvote') ? 'active' : '' }}" style="display: inline-block;"><span class="material-icons align-middle">arrow_upward</span>{{ number_format((int) $post->upvote_count, 0, '.', ' ') > 1000000 ? number_format((int) $post->upvote_count / 1000000, 1) . 'm' : (number_format((int) $post->upvote_count, 0, '.', ' ') > 1000 ? number_format((int) $post->upvote_count / 1000, 1) . 'k' : $post->upvote_count ?? 0) }}</button>
+                </form>
+                <form action="{{ $post->isRespondedBy(Auth::user(), 'downvote') ? route('post.response.destroy') : route('post.response') }}" method="post" style="display: inline-block;">
+                    @csrf
+                    @if($post->isRespondedBy(Auth::user(), 'downvote'))
+                        @method('DELETE')
+                    @else
+                        @method('POST')
+                    @endif
+                    <input type="hidden" name="post_id" value="{{ $post->id }}">
+                    <input type="hidden" name="action" value="downvote">
+                    <button type="submit" class="btn btn-outline-danger {{ $post->isRespondedBy(Auth::user(), 'downvote') ? 'active' : '' }}" style="display: inline-block;"><span class="material-icons align-middle">arrow_downward</span>{{ number_format((int) $post->downvote_count, 0, '.', ' ') > 1000000 ? number_format((int) $post->downvote_count / 1000000, 1) . 'm' : (number_format((int) $post->downvote_count, 0, '.', ' ') > 1000 ? number_format((int) $post->downvote_count / 1000, 1) . 'k' : $post->downvote_count ?? 0) }}</button>
+                </form>
+                <form action="{{ $post->isRespondedBy(Auth::user(), 'star') ? route('post.response.destroy') : route('post.response') }}" method="post" style="display: inline-block;">
+                    @csrf
+                    @if($post->isRespondedBy(Auth::user(), 'star'))
+                        @method('DELETE')
+                    @else
+                        @method('POST')
+                    @endif
+                    <input type="hidden" name="post_id" value="{{ $post->id }}">
+                    <input type="hidden" name="action" value="star">
+                    <button type="submit" class="btn btn-outline-warning {{ $post->isRespondedBy(Auth::user(), 'star') ? 'active' : '' }}" style="display: inline-block;"><span class="material-icons align-middle">star</span>{{ number_format((int) $post->star_count, 0, '.', ' ') > 1000000 ? number_format((int) $post->star_count / 1000000, 1) . 'm' : (number_format((int) $post->star_count, 0, '.', ' ') > 1000 ? number_format((int) $post->star_count / 1000, 1) . 'k' : $post->star_count ?? 0) }}</button>
+                </form>
+                <form action="{{ $post->isRespondedBy(Auth::user(), 'save') ? route('post.response.destroy') : route('post.response') }}" method="post" style="display: inline-block;">
+                    @csrf
+                    @if($post->isRespondedBy(Auth::user(), 'save'))
+                        @method('DELETE')
+                    @else
+                        @method('POST')
+                    @endif
+                    <input type="hidden" name="post_id" value="{{ $post->id }}">
+                    <input type="hidden" name="action" value="save">
+                    <button type="submit" class="btn btn-outline-success {{ $post->isRespondedBy(Auth::user(), 'save') ? 'active' : '' }}" style="display: inline-block;"><span class="material-icons align-middle">bookmark</span>{{ number_format((int) $post->save_count, 0, '.', ' ') > 1000000 ? number_format((int) $post->save_count / 1000000, 1) . 'm' : (number_format((int) $post->save_count, 0, '.', ' ') > 1000 ? number_format((int) $post->save_count / 1000, 1) . 'k' : $post->save_count ?? 0) }}</button>
+                </form>
+                @if($posts)
+                <a href="/post/{{ $post->id }}" class="btn btn-outline-primary me-3 align-middle d-flex align-items-center">
+                    <span class="material-icons align-middle">description</span>
+                </a>
+                @endif
+                @if ($commentDropdown)
+                <button class="btn btn-outline-primary align-middle d-flex align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#commentSection-{{ $post->id }}" aria-expanded="false" aria-controls="commentSection-{{ $post->id }}">
+                    <span class="material-icons align-middle">comment</span>
+                </button>
+                @endif
+                @if ($post->user_id == Auth::id())
+                <form action="{{ url('/post/delete') }}" method="post" style="display: inline-block;">
+                    @csrf
+                    @method('DELETE')
+                    <input type="hidden" name="post" value="{{ $post->id }}">
+                    <button type="submit" class="btn btn-outline-danger align-middle d-flex align-items-center">
+                        <span class="material-icons align-middle">delete_outline</span>
+                    </button>
+                </form>
+                @endif
+            </div>        
+        </div>
     <!-- Logika untuk commentDropdown -->
     @if ($commentDropdown)
     <div class="collapse" id="commentSection-{{ $post->id }}">
